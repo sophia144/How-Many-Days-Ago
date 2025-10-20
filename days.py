947a5bd289dd907c659d660a6e7e1bb6ed45cc48
sophia144 <piercesp@outlook.com>
2025-10-20 11:08:33 +0100

    Added functionality to read dates from a CSV

--- days.py
+++ days.py
@@ -1,11 +1,21 @@
 import numpy as np
+import csv
 
-def calculate_days_ago():
-    user_date = np.datetime64(str(input('Input your desired date in the format YYYY-MM-DD: \n')))
+def basic_date_calculation(reference_date):
     today = np.datetime64('today')
+    days_in_past = today - reference_date
+    print(f'{reference_date} was {days_in_past} ago')
 
-    days_in_past = today - user_date
+def user_input_version():
+    user_date = np.datetime64(str(input('Input your desired date in the format YYYY-MM-DD: \n')))
+    basic_date_calculation(user_date)
+
+def csv_input_calculation(file_name):
     print('\n')
-    print(f'{user_date} was {days_in_past} ago')
+    with open(file_name) as csv_file:
+        read_csv = csv.reader(csv_file, delimiter= '\n')
+        for row in read_csv:
+            basic_date_calculation(np.datetime64(row[0]))
+    print('\n')
 
-calculate_days_ago()
+csv_input_calculation('random_dates.csv')
