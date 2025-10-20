f60a90fc12fc7de04a66990ce1216c418d0ad2d6
sophia144 <piercesp@outlook.com>
2025-10-20 10:51:59 +0100

    Built basic calculation logic

--- days.py
+++ days.py
@@ -1,1 +1,10 @@
+import numpy as np
 
+def calculate_days_since():
+    user_date = np.datetime64(str(input('Input your desired date in the format YYYY-MM-DD: \n')))
+    today = np.datetime64('today')
+
+    days_in_past = today - user_date
+    print(days_in_past)
+
+calculate_days_since()
